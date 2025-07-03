68d8b688eb70256281619b785f53c2b56ab85bb5
Keval G <ganganikeval@gmail.com>
2025-07-03 17:23:56 +0530

    Refactor Home component to improve modal structure and enhance form layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 'use client';
 
-import { Button, Checkbox, Container, Modal, SimpleGrid, Textarea, TextInput } from '@mantine/core';
+import {
+  Button,
+  Card,
+  Checkbox,
+  Container,
+  Modal,
+  SimpleGrid,
+  Text,
+  Textarea,
+  TextInput,
+} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
 export default function Home() {
@@ -8,7 +18,7 @@ export default function Home() {
 
   return (
     <div>
-      <Modal opened={opened} onClose={close} title="Authentication">
+      <Modal opened={opened} onClose={close} title="Form inside modal" centered>
         <form onSubmit={(e) => e.preventDefault()}>
           <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="md" mb="md">
             <TextInput
@@ -37,30 +47,11 @@ export default function Home() {
         </Button>
       </Container>
       <Container mt="xl" mb="xl">
-        <form onSubmit={(e) => e.preventDefault()}>
-          <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="md" mb="md">
-            <TextInput
-              label="First Name"
-              placeholder=""
-              name="firstName"
-              radius="md"
-              variant="default"
-            />
-            <TextInput
-              label="Password"
-              placeholder=""
-              name="lastName"
-              radius="md"
-              variant="default"
-              type="password"
-            />
-          </SimpleGrid>
+        <Card title="Ignore this Form" shadow="sm" padding="lg" radius="md" withBorder>
+          <Text size="sm" c="red">
+            This form have password so it should not create any leads.
+          </Text>
 
-          <Button type="submit">Login</Button>
-        </form>
-      </Container>
-      <Container mt="xl" mb="xl">
-        <div className="order-1 lg:order-none bg-white rounded-2xl p-10 shadow">
           <form onSubmit={(e) => e.preventDefault()}>
             <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="md" mb="md">
               <TextInput
@@ -69,57 +60,87 @@ export default function Home() {
                 name="firstName"
                 radius="md"
                 variant="default"
-                required
               />
               <TextInput
-                label="Last Name"
+                label="Password"
                 placeholder=""
                 name="lastName"
                 radius="md"
                 variant="default"
-                required
+                type="password"
               />
             </SimpleGrid>
 
-            <TextInput
-              label="Email"
-              placeholder=""
-              type="email"
-              name="email"
-              radius="md"
-              variant="default"
-              required
-              mb="md"
-            />
-
-            <TextInput
-              label="Your Mobile Number"
-              placeholder=""
-              name="mobile"
-              radius="md"
-              variant="default"
-              mb="md"
-            />
-
-            <Textarea
-              label="Tell Us Something"
-              placeholder=""
-              name="message"
-              rows={5}
-              radius="md"
-              variant="default"
-              mb="md"
-            />
-
-            <Checkbox
-              required
-              label="I agree with the processing of personal data."
-              name="check"
-              mb="md"
-            />
-
-            <Button type="submit">Let's Talk</Button>
+            <Button type="submit">Login</Button>
           </form>
+        </Card>
+      </Container>
+      <Container mt="xl" mb="xl">
+        <div className="order-1 lg:order-none bg-white rounded-2xl p-10 shadow">
+          <Card title="Ignore this Form" shadow="sm" padding="lg" radius="md" withBorder>
+            <Text size="sm" c="green">
+              This form should create lead.
+            </Text>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="md" mb="md">
+                <TextInput
+                  label="First Name"
+                  placeholder=""
+                  name="firstName"
+                  radius="md"
+                  variant="default"
+                  required
+                />
+                <TextInput
+                  label="Last Name"
+                  placeholder=""
+                  name="lastName"
+                  radius="md"
+                  variant="default"
+                  required
+                />
+              </SimpleGrid>
+
+              <TextInput
+                label="Email"
+                placeholder=""
+                type="email"
+                name="email"
+                radius="md"
+                variant="default"
+                required
+                mb="md"
+              />
+
+              <TextInput
+                label="Your Mobile Number"
+                placeholder=""
+                name="mobile"
+                radius="md"
+                variant="default"
+                mb="md"
+              />
+
+              <Textarea
+                label="Tell Us Something"
+                placeholder=""
+                name="message"
+                rows={5}
+                radius="md"
+                variant="default"
+                mb="md"
+              />
+
+              <Checkbox
+                required
+                label="I agree with the processing of personal data."
+                name="check"
+                mb="md"
+              />
+
+              <Button type="submit">Let's Talk</Button>
+            </form>
+          </Card>
         </div>
       </Container>
     </div>

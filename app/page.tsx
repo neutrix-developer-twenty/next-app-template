7787e8ed3dd63ba08e960754cf5b3c0a4e702846
Keval G <ganganikeval@gmail.com>
2025-07-03 17:42:05 +0530

    Enhance modal content and improve text clarity in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,12 @@ export default function Home() {
   return (
     <div>
       <Modal opened={opened} onClose={close} title="Form inside modal" centered>
+        <b>Handle dynamic content:</b>
+        <Text size="sm" c="green" mb="md" fw="bold">
+          if forms are added later (SPA content, modal pop-ups, etc.), the script should notice and
+          start tracking them as well.
+        </Text>
+
         <form onSubmit={(e) => e.preventDefault()}>
           <SimpleGrid cols={{ base: 1, lg: 2 }} spacing="md" mb="md">
             <TextInput
@@ -50,7 +56,7 @@ export default function Home() {
       </Container>
       <Container mt="xl" mb="xl">
         <Card title="Ignore this Form" shadow="sm" padding="lg" radius="md" withBorder>
-          <Text size="sm" c="red">
+          <Text size="sm" c="red" fw="bold">
             This form have password so it should not create any leads.
           </Text>
 
@@ -80,7 +86,7 @@ export default function Home() {
       <Container mt="xl" mb="xl">
         <div className="order-1 lg:order-none bg-white rounded-2xl p-10 shadow">
           <Card title="Ignore this Form" shadow="sm" padding="lg" radius="md" withBorder>
-            <Text size="sm" c="green">
+            <Text size="sm" c="green" fw="bold">
               This form should create lead.
             </Text>
             <form onSubmit={(e) => e.preventDefault()}>
@@ -150,7 +156,7 @@ export default function Home() {
         <div className="order-1 lg:order-none bg-white rounded-2xl p-10 shadow">
           <Card title="Ignore this Form" shadow="sm" padding="lg" radius="md" withBorder>
             <b>Handling the page unload case:</b>
-            <Text size="sm" c="green" mb="md">
+            <Text size="sm" c="green" mb="md" fw="bold">
               If a form causes a full page reload (typical for a non-AJAX {`<form action="...">`}),
               our handler will execute just before the unload. We must send the data quickly. We
               will leverage the same technique already used in the script for sending data on
@@ -190,7 +196,7 @@ export default function Home() {
                 mb="md"
               />
 
-              <Button type="submit">Let's Talk</Button>
+              <Button type="submit">Submit</Button>
             </form>
           </Card>
         </div>
